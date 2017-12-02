62cbb1f0905a96323c668b8291b3bd37e26afeb8
heiheychan <cchei1993@gmail.com>
2017-12-01 22:44:44 -0500

    Set proxy true

--- config/keys.js
+++ config/keys.js
@@ -3,11 +3,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./dev');
 }
-
-
-
-// Client ID  
-// 375487214965-indctf3fnj1p5tbkf9t9p22uj9r815hf.apps.googleusercontent.com
-// Client secret 
-// JP6NU_5UvxgvKIPvd6KPj-Ag
-// mongodb://bill:password@ds141175.mlab.com:41175/node-emaily-prod
