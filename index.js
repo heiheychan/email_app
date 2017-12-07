721aa4b55cc4fd81e4ffe548f990a7b4cc34708b
heiheychan <cchei1993@gmail.com>
2017-12-06 20:52:54 -0500

    Add billing and client side app

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys')
 require('./models/User');
 require('./services/passport');
 
+// Set up Database
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// Body parser & Cookie parser
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, //Last for 30 days
@@ -17,10 +21,25 @@ app.use(
   })
 );
 
+// Passport initialization
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Routes
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve up static files
+  app.use(express.static('client/build'));
+
+  // Serve up the index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  })
+};
+
+// Start listening!
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
